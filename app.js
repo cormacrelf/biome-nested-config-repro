1d4f363f8382345a4ce323da44162cbc7c79653b
Cormac Relf <web@cormacrelf.net>
2025-11-24 13:15:04 +1100

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let    x = 5;
+
+function indent() {
+    // `biome format .` respects the formatter/indentWidth property from the root config (4 spaces)
+    return x;
+}
---
+++ subdirectory/lib.ts
@@ -0,0 +1,6 @@
+let y              = 65;
+
+function indent() {
+  // `biome format .` respects the formatter/indentWidth property from the local config (2 spaces)
+  return y;
+}
